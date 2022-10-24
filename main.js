07905dc7fdd37a8821e1f538fe5ea8657a019ea7
Spageses <86549403+Spageses@users.noreply.github.com>
2022-10-24 10:19:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ rightWristY = 0;
 scoreLeftWrist = 0;
 scoreRightWrist = 0;
 
-function preload(){
+function preload() {
     music_1 = loadSound("music.mp3");
     music_2 = loadSound("music2.mp3");
 }
 
-function setup(){
+function setup() {
     canvas = createCanvas(600, 300);
     canvas.center();
 
@@ -47,6 +47,20 @@ function gotPoses(results) {
     }
 }
 
-function draw(){
-    image(video, 0, 0, 600, 300);
+function draw() {
+    image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        song.stop(music_2)
+
+        if (music_1.isPlaying()) {
+            song.play(music_1);
+            document.getElementById("song_name").innerHTML = "Song  = Harry Potter Theme Song(Remix)";
+        }
+
+    }
 }
